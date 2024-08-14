2c96093d9a0bbe19e37f30104301b5468469f0ae
SANGHYYUN0519 <tkdgus05197@gmail.com>
2024-08-14 16:16:38 +0900

    java_project / 1차

---
+++ java.project/src/main/java/com/ohgiraffers/Application.java
@@ -0,0 +1,37 @@
+package com.ohgiraffers;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+        while(true) {
+            System.out.println("==== HR 대행 프로그램 ====");
+            System.out.println(" 1. 모든 오퍼리스트 조회 ");
+            System.out.println(" 2. 오퍼 정보 조회 ");
+            System.out.println(" 3. 오퍼 등록 ");
+            System.out.println(" 4. 오퍼 수정 ");
+            System.out.println(" 5. 오퍼 삭제");
+            System.out.println(" 9. 프로그램 종료");
+            System.out.println(" 서비스 선택 : ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1: break;
+                case 2: break;
+                case 3: break;
+                case 4: break;
+                case 5: break;
+                case 9: break;
+
+            }
+
+
+        }
+
+    }
+}
---
+++ java.project/src/main/java/com/ohgiraffers/OfferInfo.java
@@ -0,0 +1,109 @@
+package com.ohgiraffers;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class OfferInfo {
+    private String companyName;
+    private String position;
+    private String location;
+    private int salary;
+    private String jobDescription;
+    private String[] welfare;
+    private int jobCode;
+
+    public OfferInfo() {}   // 기본생성자
+
+    public OfferInfo(String companyName, String position, String location, int salary, String jobDescription, String[] welfare, int jobCode) {
+        this.companyName = companyName;
+        this.position = position;
+        this.location = location;
+        this.salary = salary;
+        this.jobDescription = jobDescription;
+        this.welfare = welfare;
+        this.jobCode = jobCode;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public void setJobDescription(String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
+
+    public String[] getWelfare() {
+        return welfare;
+    }
+
+    public void setWelfare(String[] welfare) {
+        this.welfare = welfare;
+    }
+
+
+    public int getJobCode() {
+        return jobCode;
+    }
+
+    public void setJobCode(int jobCode) {
+        this.jobCode = jobCode;
+    }
+
+    @Override
+    public String toString() {
+        return "OfferInfo{" +
+                "companyName='" + companyName + '\'' +
+                ", position='" + position + '\'' +
+                ", location='" + location + '\'' +
+                ", salary=" + salary +
+                ", jobDescription='" + jobDescription + '\'' +
+                ", welfare=" + Arrays.toString(welfare) +
+                ", jobCode=" + jobCode +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OfferInfo offerInfo = (OfferInfo) o;
+        return salary == offerInfo.salary && jobCode == offerInfo.jobCode && Objects.equals(companyName, offerInfo.companyName) && Objects.equals(position, offerInfo.position) && Objects.equals(location, offerInfo.location) && Objects.equals(jobDescription, offerInfo.jobDescription) && Objects.deepEquals(welfare, offerInfo.welfare);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(companyName, position, location, salary, jobDescription, Arrays.hashCode(welfare), jobCode);
+    }
+}
